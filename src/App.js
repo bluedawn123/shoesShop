3180bd2a829b60d0211b5f58246cc21a0450c56a
bluedawn123 <haemilyjh@naver.com>
2024-12-30 20:17:06 +0900

    컴포넌트하고 map으로 축약 + 기타 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import data from './data.js';
+import { useState } from 'react';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className="row">
+        {/* <Card shoes={shoes[0]} i={1} />
+        <Card shoes={shoes[1]} i={2} />
+        <Card shoes={shoes[2]} i={3} /> */}
+        {
+          shoes.map( (a, i) => {
+            return(
+              <Card shoes={shoes[i]} i={i} />
+            )
+          })
+        }
+        </div>
+      </div>
     </div>
   );
+}
+
+function Card(props){
+  return(
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i +1) + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
 }
 
 export default App;
